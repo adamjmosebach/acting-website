69b780481eb3bfa48bff9f953bfbfff332edd71b
Adam Mosebach <adam_mosebach@adams-mbp.myfiosgateway.com>
2021-04-07 19:45:18 -0400

    bio page

--- src/screens/Bio.jsx
+++ src/screens/Bio.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Bio.css'
+import bioPic from '../images/bioPic.jpg'
 
 function Bio({ updateBannerNav }) {
 
@@ -7,7 +8,18 @@ function Bio({ updateBannerNav }) {
 
   return (
     <div className='bio-div'>
-      Bio
+      <img src={bioPic} alt='bio-pic' className='bio-pic'></img>
+      <div className='bio-container'>
+        <div className='bio'>
+          <p>Hi, I'm Adam!</p>
+          <p>When I was in fifth grade, my father came into the living room, newspaper in hand, and told me that the local community theatre was having auditions for a show called "Oliver".  He said I looked just like the kid in the movie (I did at the time) and told me I had to audition.</p>
+          <p>My response..."No way!"  Sure I'd done the second-grade play, but being on stage was not something I really cared about.  But he said he really wanted me to try out and see how it goes.  Well, I did.  I sang a song a capella, had to do an improv scene, and then was asked to read through a scene and come back later in the day to have a callback.</p>
+          <p>That was the moment something inside me snapped.  I suddenly reeeaaallly wanted that part.  I read, re-read, and rehearsed the scene with my brother over and over again right up until the moment I had to go back in.  I went in, nailed it, got the part, and was bitten by the bug.</p>
+          <p>Since then, I have made a career out of acting - having been in over 170 productions. I've performed in NYC, at Avery Fisher Hall, in many regional productions, have been in national and international tours, had roles on TV and the internet, and am an EMC and a member of SAG-AFTRA.</p>
+          <p>And just to put all rumors to rest...</p>
+          <p>It's true what you've heard...I eat almost exclusively pasta.  I'm ok with that.</p>
+        </div>
+      </div>
     </div>
   )
 }
