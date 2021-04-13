dd52e701d6cd6bd96fb4a3b65f75d198be6db070
Adam Mosebach <adam_mosebach@adams-mbp.myfiosgateway.com>
2021-04-12 21:25:19 -0400

    media in - home nav btns tweak

--- src/screens/Media.jsx
+++ src/screens/Media.jsx
@@ -7,8 +7,10 @@ function Media({ updateBannerNav }) {
 
   return (
     <div className='media-div'>
-      Media
       <iframe title='gethsemane' src="https://www.youtube.com/embed/qnaIGRZyYaw" ></iframe>
+      <iframe title='you tore my hear' src='https://www.youtube.com/embed/KVbQdz7ASNI' ></iframe>
+      <iframe title='a world without you' src='https://www.youtube.com/embed/zjIRkOzkJvk' ></iframe>
+      <iframe title='all good gifts' src='https://www.youtube.com/embed/3Xc6eGmA-Ws' ></iframe>
     </div>
   )
 }
