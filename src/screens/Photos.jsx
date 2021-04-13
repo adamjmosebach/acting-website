28b6d8f649f64a46ea4fb0c8cc9a922c52da4f03
Adam Mosebach <adam_mosebach@adams-mbp.myfiosgateway.com>
2021-04-13 18:15:14 -0400

    first photos and structure up

--- src/screens/Photos.jsx
+++ src/screens/Photos.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import './Photos.css'
+import anne1 from '../images/anne1.jpeg'
+import anne2 from '../images/anne2.jpeg'
+import anne3 from '../images/anne3.jpeg'
+import anne4 from '../images/anne4.jpeg'
+
 
 function Photos({ updateBannerNav }) {
 
   updateBannerNav(true);
 
   return (
-    <div className='photos-div'>
-      Photos
+    <div className='photo-album'>
+      <h4>Anne of Green Gables</h4>
+      <div className='photo-section'>
+        <img src={anne1} className='photo-album-photo vert' alt='anne of green gables'></img>
+        <img src={anne2} className='photo-album-photo horiz' alt='anne of green gables'></img>
+        <img src={anne3} className='photo-album-photo horiz' alt='anne of green gables'></img>
+        <img src={anne4} className='photo-album-photo vert' alt='anne of green gables'></img>
+      </div>
     </div>
   )
 }
