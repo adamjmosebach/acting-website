e7665d1e4fc3b7868df98daa6af27134877de3cc
Adam Mosebach <adam_mosebach@adams-mbp.myfiosgateway.com>
2022-10-03 21:52:43 -0400

    fix pic heights and spacing for phone

--- src/screens/Bio.jsx
+++ src/screens/Bio.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import './Bio.css'
-import bioPic from '../images/bioPic.jpg'
-import signature from '../images/signatureFirstName.png'
+import React from 'react';
+import './Bio.css';
+import bioPic from '../images/bioPic.jpg';
+import signature from '../images/signatureFirstName.png';
 
 function Bio({ updateBannerNav, setCurrentPage }) {
-
   updateBannerNav(true);
   setCurrentPage('Bio');
 
@@ -12,20 +11,46 @@ function Bio({ updateBannerNav, setCurrentPage }) {
     <div className='bio-div'>
       <img src={bioPic} alt='bio-pic' className='bio-pic'></img>
       <div className='bio-container'>
-        <div className='bio'>
-          <p className='bio-hi'><strong>Hi, I'm Adam!</strong></p>
-          <p>When I was in fifth grade, my father came into the living room, newspaper in hand, and told me that the local community theatre was having auditions for a show called "Oliver".  He said I looked just like the kid in the movie (I did at the time) and told me I had to audition.</p>
-          <p>My response..."No way!"  Sure I'd done the second-grade play, but being on stage was not something I really cared about.  But he said he really wanted me to try out and see how it goes.  Well, I did.  I sang a song a capella, had to do an improv scene, and then was asked to read through a scene and come back later in the day to have a callback.</p>
-          <p>That was the moment something inside me snapped.  I suddenly reeeaaallly wanted that part.  I read, re-read, and rehearsed the scene with my brother over and over again right up until the moment I had to go back in.  I went in, nailed it, got the part, and was bitten by the bug.</p>
-          <p>Since then, I have made a career out of acting - having been in over 170 productions. I've performed in NYC, at Avery Fisher Hall, in many regional productions, have been in national and international tours, had roles on TV and the internet, and am an EMC and a member of SAG-AFTRA.</p>
-          <p>And just to put all rumors to rest...</p>
-          <p>It's true what you've heard...I eat almost exclusively pasta.  I'm ok with that.</p>
-          <p>Hope to see you soon,</p>
-          <img src={signature} alt='signature' className='signature'></img>
-        </div>
+        <h2 className='bio-hi'>
+          <strong>Hi, I'm Adam!</strong>
+        </h2>
+        <p>
+          When I was in fifth grade, my father came into the living room,
+          newspaper in hand, and told me that the local community theatre was
+          having auditions for a show called "Oliver". He said I looked just
+          like the kid in the movie (I did at the time) and told me I had to
+          audition.
+        </p>
+        <p>
+          My response..."No way!" Sure I'd done the second-grade play, but being
+          on stage was not something I really cared about. But he said he really
+          wanted me to try out and see how it goes. Well, I did. I sang a song a
+          capella, had to do an improv scene, and then was asked to read through
+          a scene and come back later in the day to have a callback.
+        </p>
+        <p>
+          That was the moment something inside me snapped. I suddenly
+          reeeaaallly wanted that part. I read, re-read, and rehearsed the scene
+          with my brother over and over again right up until the moment I had to
+          go back in. I went in, nailed it, got the part, and was bitten by the
+          bug.
+        </p>
+        <p>
+          Since then, I have made a career out of acting - having been in over
+          170 productions. I've performed in NYC, at Avery Fisher Hall, in many
+          regional productions, have been in national and international tours,
+          had roles on TV and the internet, and am an EMC and a member of
+          SAG-AFTRA.
+        </p>
+        <p>
+          And just to put all rumors to rest... It's true what you've heard...I
+          eat almost exclusively pasta. I'm ok with that.
+        </p>
+        <p>Hope to see you soon,</p>
+        <img src={signature} alt='signature' className='signature'></img>
       </div>
     </div>
-  )
+  );
 }
 
-export default Bio
+export default Bio;
