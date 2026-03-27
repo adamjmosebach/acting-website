a0605413f51a8030dbd5928b4277acdcfad28527
Adam Mosebach <adam.mosebach@incompassapp.com>
2026-03-26 21:42:38 -0400

    redesign banner height, headshot (bearded), phone responsive

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,29 +1,33 @@
-import './Home.css'
-import headshot from '../images/headshot.jpg'
-import MOTOEflyer from '../images/MOTOEflyer.jpg'
-import HomeNavButton from '../components/HomeNavButton'
+import "./Home.css";
+// import headshot from "../images/headshot.jpg";
+import headshot from "../images/headshot-bearded-darker-background.png";
+import MOTOEflyer from "../images/MOTOEflyer.jpg";
+import HomeNavButton from "../components/HomeNavButton";
 
 function Home({ updateBannerNav }) {
-
-  updateBannerNav(false)
+  updateBannerNav(false);
 
   return (
-    <div className='home-div'>
-      <div className='home-nav'>
-        <HomeNavButton name='Bio' linkTo='/bio' />
-        <HomeNavButton name='Resume' linkTo='/resume' />
-        <HomeNavButton name='Photos' linkTo='/photos' />
-        <HomeNavButton name='Media' linkTo='/media' />
-        <HomeNavButton name='Contact' linkTo='/contact' />
+    <div className="home-div">
+      <div className="home-group-for-phones">
+        <div className="home-nav">
+          <HomeNavButton name="Bio" linkTo="/bio" />
+          <HomeNavButton name="Resume" linkTo="/resume" />
+          <HomeNavButton name="Photos" linkTo="/photos" />
+          <HomeNavButton name="Media" linkTo="/media" />
+          <HomeNavButton name="Contact" linkTo="/contact" />
+        </div>
+        <div className="headshot-container">
+          <img src={headshot} alt="headshot" className="headshot"></img>
+        </div>
       </div>
-      <div className='headshot-container'>
-        <img src={headshot} alt='headshot' className='headshot'></img>
-      </div>
-      <div className='headshot-container'>
-        <a href='https://cur8.com/21064/project/135538'><img src={MOTOEflyer} alt='headshot' className='headshot'></img></a>
-      </div>
+      {/* <div className="current-show-container">
+        <a href="https://cur8.com/21064/project/135538" target="_blank">
+          <img src={MOTOEflyer} alt="headshot" className="current-show"></img>
+        </a>
+      </div> */}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
